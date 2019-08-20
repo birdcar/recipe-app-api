6200b5eae9b4af62ec36eac48cf08fbbe649fdd1
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-08-20 12:24:41 -0500

    Enable token authentication view

--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
@@ -8,3 +10,11 @@ class CreateUserView(generics.CreateAPIView):
     An endpoint to create a new user
     """
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """
+    An endpoint to create a new auth token for existing users
+    """
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
