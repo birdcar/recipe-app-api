9045f1eeb64f95792712030c7e36f0644d270bd4
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-08-22 12:59:36 -0500

    Define ViewSet for Tag model

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from core.models import Tag
+from .serializers import TagSerializer
+
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """
+    List views for the Tag resource
+    """
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    def get_queryset(self):
+        """
+        Return tags for the current authenticated user only
+        """
+        return self.queryset.filter(user=self.request.user)
