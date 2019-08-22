22c66e4d36235d2a5cf8a8fa2406f414f2c95a08
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-08-22 12:20:20 -0500

    Create the Tag model and add it to the admin

--- app/core/models.py
+++ app/core/models.py
@@ -1,12 +1,14 @@
-from django.db import models
-
+from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser, BaseUserManager, PermissionsMixin)
+from django.db import models
 
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
-        """Creates and saves a new user"""
+        """
+        Creates and saves a new user
+        """
         user = self.model(email=self.normalize_email(email), **extra_fields)
         if not email:
             raise ValueError("Users must have an email address")
@@ -17,7 +19,9 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password):
-        """Creates and saves a new superuser"""
+        """
+        Creates and saves a new superuser
+        """
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
@@ -27,7 +31,9 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that supports using email instead of username"""
+    """
+    Custom user model that supports using email instead of username
+    """
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -36,3 +42,14 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+
+
+class Tag(models.Model):
+    """
+    Tag object: used to classify recipes
+    """
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE)
+
+    def __str__(self):
+        return self.name
