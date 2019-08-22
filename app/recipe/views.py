f83e0e53e1f56abc56853750534e86b90aa366a5
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-08-22 16:54:47 -0500

    DRY up recipe views with a base ViewSet

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,49 +6,39 @@ from core.models import Tag, Ingredient
 from .serializers import TagSerializer, IngredientSerializer
 
 
-class TagViewSet(mixins.ListModelMixin,
-                 mixins.CreateModelMixin,
-                 viewsets.GenericViewSet):
+class BaseRecipeAttrViewSet(mixins.ListModelMixin,
+                            mixins.CreateModelMixin,
+                            viewsets.GenericViewSet):
+    """
+    A base ViewSet for Recipe resources
+    """
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """
+        Return objects for the current authenticated user only
+        """
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """
+        Create a new object
+        """
+        serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
     """
     Views related to the Tag resource
     """
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
-    def get_queryset(self):
-        """
-        Return tags for the current authenticated user only
-        """
-        return self.queryset.filter(user=self.request.user).order_by('-name')
 
-    def perform_create(self, serializer):
-        """
-        Create a new Tag
-        """
-        serializer.save(user=self.request.user)
-
-
-class IngredientViewSet(mixins.ListModelMixin,
-                        mixins.CreateModelMixin,
-                        viewsets.GenericViewSet):
+class IngredientViewSet(BaseRecipeAttrViewSet):
     """
     Views related to the Ingredient resource
     """
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
-
-    def get_queryset(self):
-        """
-        Return Ingredients for the currently authenticated user only
-        """
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """
-        Create a new Ingredient
-        """
-        serializer.save(user=self.request.user)
