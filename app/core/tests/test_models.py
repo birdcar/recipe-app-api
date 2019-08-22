c9f27cf3f9ca038464b8c69140442da08517b3af
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-08-22 15:15:27 -0500

    Add tests for the Ingredient model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -55,3 +55,14 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_dunder_str(self):
+        """
+        The __str__ method for Ingredients should be properly implemented
+        """
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name="Cucumber"
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
