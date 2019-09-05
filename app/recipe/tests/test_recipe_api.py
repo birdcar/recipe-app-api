6e6ce29b5ee91ae94f54361fe9f6441bcafb4e56
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-09-05 12:53:34 -0500

    Update recipe tests to ensure they reflect the actual app requirements

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -17,22 +17,22 @@ def sample_recipe(user, **params):
     """
     Generate a new Recipe object and return it
     """
-    default = {
+    defaults = {
         'title': 'Sample recipe',
         'time_minutes': 10,
         'price': 5.00
     }
-    default.update(params)
+    defaults.update(params)
     return Recipe.objects.create(user=user, **defaults)
 
 
-class PublicRecipeAPITests(self):
+class PublicRecipeAPITests(TestCase):
     """
     Test unauthenticated requests made to the Recipe resource
     """
 
     def setUp(self):
-        self.client = APIClient
+        self.client = APIClient()
 
     def test_require_auth(self):
         """
@@ -43,36 +43,49 @@ class PublicRecipeAPITests(self):
         self.assertTrue(status.is_client_error(res.status_code))
 
 
-class PrivateRecipeAPITests(self):
+class PrivateRecipeAPITests(TestCase):
     """
     Test authenticated requests made to the Recipe resource
     """
 
     def setUp(self):
-        self.client = APIClient
+        self.client = APIClient()
         self.user = get_user_model().objects.create_user(
             name="Testy McTester",
             email="test@example.com",
             password="testpass"
         )
-        self.client.force_authenticate(self.user)
         self.private_user = get_user_model().objects.create_user(
             name="Shawna Jean",
             email="shawna@leavemealone.com",
             password="mypasswordkeepsalltheboysfrommyyard"
         )
-
+        self.client.force_authenticate(self.user)
         self.payload_KG = sample_recipe(self.user)
-        self.payload_KB_missing_title = Recipe.objects.create(
-            title='',
-            time_minutes=10,
-            price=5.00
-        )
 
     def test_get_recipes_success(self):
         """
         Users are able to retrieve recipes
         """
+        sample_recipe(user=self.user)
+
         res = self.client.get(RECIPE_URL)
+        recipes = Recipe.objects.all().order_by('-id')
+        serializer = RecipeSerializer(recipes, many=True)
 
         self.assertTrue(status.is_success(res.status_code))
+        self.assertEqual(res.data, serializer.data)
+
+    def test_get_recipes_limited_to_user_success(self):
+        """
+        Users can only retrieve recipes associated with their account
+        """
+        sample_recipe(user=self.private_user)
+
+        res = self.client.get(RECIPE_URL)
+        recipes = Recipe.objects.filter(user=self.user)
+        serializer = RecipeSerializer(recipes, many=True)
+
+        self.assertTrue(status.is_success(res.status_code))
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data, serializer.data)
