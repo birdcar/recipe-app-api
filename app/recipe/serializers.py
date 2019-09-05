27e0000faeeb65295a10e015d7b0c6d51ba0892f
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-09-05 12:53:59 -0500

    Create Recipe model serializer

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
-from core.models import Tag, Ingredient
+from core.models import Tag, Ingredient, Recipe
 
 
 class TagSerializer(serializers.ModelSerializer):
     """
-    Serializer for Tag model objects
+    Serializer for Tag model objects in the core app
     """
 
     class Meta:
@@ -15,10 +15,30 @@ class TagSerializer(serializers.ModelSerializer):
 
 class IngredientSerializer(serializers.ModelSerializer):
     """
-    Serializer for Ingredient model objects
+    Serializer for Ingredient model objects in the core app
     """
 
     class Meta:
         model = Ingredient
         fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Recipe model objects in the core app
+    """
+    ingredients = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Ingredient.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all()
+    )
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes',
+                  'price', 'link')
         read_only_fields = ('id',)
